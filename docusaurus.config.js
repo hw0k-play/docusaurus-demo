c8577aba5d4435490b3e1f4d3d911d650be4e4b7
Hyunwook Nam <hw0knam@gmail.com>
2021-06-22 13:11:39 +0000

    fix: algolia 연동 일시 제거

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -43,10 +43,10 @@ module.exports = {
       theme: lightCodeTheme,
       darkTheme: darkCodeTheme,
     },
-    algolia: {
-      apiKey: process.env.ALGOLIA_API_KEY,
-      indexName: 'docusaurus-demo',
-    },
+    // algolia: {
+    //   apiKey: process.env.ALGOLIA_API_KEY,
+    //   indexName: 'docusaurus-demo',
+    // },
   },
   presets: [
     [
